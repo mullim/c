b1349e44d6d4f07989a05a01b0a2c29d0f0c3134
Mike Mulligan <mkmllgn@gmail.com>
2024-10-02 06:23:02 -0400

    Initial Commit

---
+++ 02_NumberInput/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int myFavoriteNumber = 0;
+
+    printf("Please enter your favorite number\n");
+    scanf("%d", &myFavoriteNumber);
+    printf("You entered: %d\n", myFavoriteNumber);
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# C
+
+## Description
+
+The code in this repository is from a [C course on Udemy](https://www.udemy.com/course/c-programming-for-beginners).
